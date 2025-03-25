c083a31956ef1b30360dfaf35bab1289db64ca30
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2025-03-24 22:27:53 -0400

    updated faq etc

--- src/Components/FAQ/index.tsx
+++ src/Components/FAQ/index.tsx
@@ -1,77 +1,108 @@
-import React, { useState } from 'react';
-import './FAQ.scss';
-import { ChevronDown } from 'lucide-react';
-
+import React, { useState } from "react";
+import "./FAQ.scss";
+import { ChevronDown } from "lucide-react";
 
 interface FAQItem {
   question: string;
   answer: string | React.ReactNode;
 }
 
-
 const faqData: FAQItem[] = [
   {
     question: "What is a hackathon?",
-    answer: "A Hackathon is a 24-hour tech oriented event where students can come together to create amazing software and/or hardware projects. Come with an awesome idea, a great team, a positive attitude, or all of the above!"
+    answer:
+      "A Hackathon is a 24-hour tech oriented event where students can come together to create amazing software and/or hardware projects. Come with an awesome idea, a great team, a positive attitude, or all of the above!",
   },
   {
     question: "Who can participate?",
-    answer: "Any current college student! No experience necessary: you'll have the chance to pick up new skills from our workshops during the hackathon."
+    answer:
+      "Any current college student! No experience necessary: you'll have the chance to pick up new skills from our workshops during the hackathon.",
   },
   {
     question: "How do teams work?",
-    answer: "You can work on a project on your own, or you can work with a team. While teams can be specified on the application, you won't officially create your team until the event. We hold a team formation social before the hacking begins so it is totally okay to show up even if you don’t know anybody!"
+    answer:
+      "You can work on a project on your own, or you can work with a team. While teams can be specified on the application, you won't officially create your team until the event. We hold a team formation social before the hacking begins so it is totally okay to show up even if you don’t know anybody!",
   },
   {
     question: "What should I bring?",
-    answer: "It is recommended that you bring a computer, chargers, and a pair of headphones. Really anything that will help you feel comfortable ranging from hygiene products to blankets. Food will be provided."
+    answer:
+      "It is recommended that you bring a computer, chargers, and a pair of headphones. Really anything that will help you feel comfortable ranging from hygiene products to blankets. Food will be provided.",
   },
   {
     question: "When is the deadline to register?",
-    answer: "The application deadline for Dartmouth students is April 18th, and the deadline for anyone who is not a Dartmouth student is April 4th."
+    answer:
+      "The application deadline for Dartmouth students is April 18th, and the deadline for anyone who is not a Dartmouth student is April 4th.",
   },
   {
     question: "Will there be activities or workshops?",
-    answer: "Yes! Throughout the hackathon, we'll host various workshops, tech talks, and networking events. These provide opportunities to learn new skills and connect with sponsors and other participants."
+    answer:
+      "Yes! Throughout the hackathon, we'll host various workshops, tech talks, and networking events. These provide opportunities to learn new skills and connect with sponsors and other participants.",
   },
   {
     question: "How do I travel to HackDartmouth?",
-    answer: "HackDartmouth will be running shuttles from Boston and New York the morning of the event which will be free for Hackathon attendees! Unfortunately, due to budget constraints we are unable to offer reimbursements for individual travel."
+    answer: (
+      <>
+        <p>
+          There are shuttles from Boston and New York to the Dartmouth Campus
+          which can be purchased <a href="https://dartmouthcoach.com/">here</a>.
+          Unfortunately, due to budget constraints HackDartmouth is unable to
+          offer reimbursements for individual travel at this point. However, we
+          will update attendees if this changes.
+        </p>
+      </>
+    ),
   },
   {
     question: "What can I win?",
-    answer: "A panel of judges will decide on the winning hacks based on criteria of creativity, usefulness, technical difficulty, and form. The top prizes are TBA. Previous prizes have included drones, Apple watches, Nintendo switches, bluetooth speakers, and other awesome tech."
+    answer:
+      "A panel of judges will decide on the winning hacks based on criteria of creativity, usefulness, technical difficulty, and form. The top prizes are TBA. Previous prizes have included drones, Apple watches, Nintendo switches, bluetooth speakers, and other awesome tech.",
   },
   {
     question: "How can I receive accommodations?",
     answer: (
       <>
-        <p>Shoot us an email at <a 
-          href="mailto:dartmouthhackathon@gmail.com"
-          className="faq-link"
-        >
-          dartmouthhackathon@gmail.com
-        </a> and we’ll get back to you as soon as we can!
+        <p>
+          Shoot us an email at{" "}
+          <a href="mailto:dartmouthhackathon@gmail.com" className="faq-link">
+            dartmouthhackathon@gmail.com
+          </a>{" "}
+          and we’ll get back to you as soon as we can!
         </p>
       </>
-    )
+    ),
   },
   {
     question: "MLH code of conduct?",
     answer: (
       <>
-        <p>The MLH Code of Conduct can be found on their website, <a 
-          href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="faq-link"
-        >
-          here!
-        </a>
+        <p>
+          The MLH Code of Conduct can be found on their website,{" "}
+          <a
+            href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="faq-link"
+          >
+            here!
+          </a>
         </p>
       </>
-    )
-  }
+    ),
+  },
+  {
+    question: "What if I am interested in sponsoring the event??",
+    answer: (
+      <>
+        <p>
+          Please reach out to{" "}
+          <a href="mailto:dartmouthhackathon@gmail.com">
+            dartmouthhackathon@gmail.com
+          </a>
+          .
+        </p>
+      </>
+    ),
+  },
 ];
 
 const AccordionItem: React.FC<{
@@ -80,30 +111,28 @@ const AccordionItem: React.FC<{
   onClick: () => void;
 }> = ({ item, isOpen, onClick }) => {
   return (
-    <div className={`accordion-item ${isOpen ? 'open' : ''}`}>
+    <div className={`accordion-item ${isOpen ? "open" : ""}`}>
       <div className="accordion-header" onClick={onClick}>
         <span>{item.question}</span>
-        <div className={`accordion-icon ${isOpen ? 'rotated' : ''}`}>
+        <div className={`accordion-icon ${isOpen ? "rotated" : ""}`}>
           <ChevronDown size={24} />
         </div>
       </div>
       <div className="accordion-content">
-        {typeof item.answer === 'string' ? <p>{item.answer}</p> : item.answer}
+        {typeof item.answer === "string" ? <p>{item.answer}</p> : item.answer}
       </div>
     </div>
   );
 };
 
-
 const FAQ: React.FC = () => {
   const [openItemIndices, setOpenItemIndices] = useState<number[]>([]);
 
   const toggleAccordion = (index: number) => {
-    setOpenItemIndices(prev => {
+    setOpenItemIndices((prev) => {
       if (prev.includes(index)) {
-        return prev.filter(i => i !== index);
-      } 
-      else {
+        return prev.filter((i) => i !== index);
+      } else {
         return [...prev, index];
       }
     });
@@ -132,7 +161,9 @@ const FAQ: React.FC = () => {
               <AccordionItem
                 key={index + leftColumnItems.length}
                 item={item}
-                isOpen={openItemIndices.includes(index + leftColumnItems.length)} 
+                isOpen={openItemIndices.includes(
+                  index + leftColumnItems.length
+                )}
                 onClick={() => toggleAccordion(index + leftColumnItems.length)}
               />
             ))}
--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import './Navbar.scss';
-import hackDartmouthLogo from '~/assets/hackDartmouthLogo.svg'
-import MLHLogo from '~/assets/MLHLogo.svg'
-
+import React, { useState } from "react";
+import "./Navbar.scss";
+import hackDartmouthLogo from "~/assets/hackDartmouthLogo.svg";
+import MLHLogo from "~/assets/MLHLogo.svg";
 
 const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -16,10 +15,13 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className={`navbar ${menuOpen ? 'menu-open' : ''}`}>
+    <nav className={`navbar ${menuOpen ? "menu-open" : ""}`}>
       <div className="navbar__container">
-
-        <button className="navbar__hamburger" onClick={toggleMenu} aria-label="Toggle menu">
+        <button
+          className="navbar__hamburger"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
           <div></div>
           <div></div>
           <div></div>
@@ -31,27 +33,58 @@ const Navbar: React.FC = () => {
             <span>HackDartmouth</span>
           </div>
         </div>
-        
+
         <div className="navbar__right">
-          <a href="#about" className="link">About</a>
-          <a href="https://forms.gle/wxrraAvjLn9d6gP7A" className="link" target="_blank" rel="noopener noreferrer">Apply</a>
-          <a href="#faq" className="link">FAQ</a>
-          <a href="#contact" className="link">Contact</a>
+          <a href="#about" className="link">
+            About
+          </a>
+          <a
+            href="https://forms.gle/wxrraAvjLn9d6gP7A"
+            className="link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Apply
+          </a>
+          <a href="#faq" className="link">
+            FAQ
+          </a>
+          <a href="#contact" className="link">
+            Contact
+          </a>
           <div className="navbar__logo-right">
             <div className="mlh-badge">
-              <img src={MLHLogo} alt="MLH Official 2025 Season" />
+              <a href="https://mlh.io/seasons/2025/events" target="_BLANK">
+                <img src={MLHLogo} alt="MLH Official 2025 Season" />
+              </a>
             </div>
           </div>
         </div>
       </div>
 
-      <div className={`navbar__overlay ${menuOpen ? 'open' : ''}`} onClick={closeMenu}></div>
-      
-      <div className={`navbar__mobile-menu ${menuOpen ? 'open' : ''}`}>
-        <a href="#about" className="link" onClick={closeMenu}>About</a>
-        <a href="https://forms.gle/wxrraAvjLn9d6gP7A" className="link" target="_blank" rel="noopener noreferrer">Apply</a>
-        <a href="#faq" className="link" onClick={closeMenu}>FAQ</a>
-        <a href="#contact" className="link" onClick={closeMenu}>Contact</a>
+      <div
+        className={`navbar__overlay ${menuOpen ? "open" : ""}`}
+        onClick={closeMenu}
+      ></div>
+
+      <div className={`navbar__mobile-menu ${menuOpen ? "open" : ""}`}>
+        <a href="#about" className="link" onClick={closeMenu}>
+          About
+        </a>
+        <a
+          href="https://forms.gle/wxrraAvjLn9d6gP7A"
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Apply
+        </a>
+        <a href="#faq" className="link" onClick={closeMenu}>
+          FAQ
+        </a>
+        <a href="#contact" className="link" onClick={closeMenu}>
+          Contact
+        </a>
         <div className="navbar__logo-right">
           <div className="mlh-badge">
             <img src={MLHLogo} alt="MLH Official 2025 Season" />
