ab399788f59208600ca670dc26f890e245916f24
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2025-04-10 16:24:21 -0400

    added sponsor images

--- src/Components/Sponsors/index.tsx
+++ src/Components/Sponsors/index.tsx
@@ -4,28 +4,44 @@ import "./Sponsors.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const sponsorPlaceholders = [1, 2, 3, 4, 5, 6];
+const sponsorPlaceholders = [
+  "/RunpodLogo.svg",
+  "/ThayerLogo.png",
+  "/NeuralinkLogo.svg",
+  "/MavisLogo.png",
+  "/RunpodLogo.svg",
+  "/ThayerLogo.png",
+  "/NeuralinkLogo.svg",
+  "/MavisLogo.png",
+];
 
 const Sponsors: React.FC = () => {
   const settings = {
     dots: false,
     infinite: true,
     speed: 5000,
-    slidesToShow: 4.5,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 0,
     cssEase: "linear",
     arrows: false,
+    responsive: [],
   };
 
   return (
     <section className="sponsors" id="sponsors">
       <h1 className="sponsors-title">Sponsors</h1>
       <Slider {...settings}>
-        {sponsorPlaceholders.map((placeholder) => (
-          <div key={placeholder} className="sponsor-slide">
-            <div className="sponsor-image-placeholder"></div>
+        {sponsorPlaceholders.map((imgPath) => (
+          <div key={imgPath} className="sponsor-slide">
+            <div className="sponsor-image-container">
+              <img
+                className="sponsor-image"
+                src={imgPath}
+                alt="Sponsor Logo"
+              />
+            </div>
           </div>
         ))}
       </Slider>
